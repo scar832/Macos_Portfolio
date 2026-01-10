3c7e69dc682663d3e84f6f7399ee196984fcaa43
scar832 <sedemboafo@gmail.com>
2026-01-10 16:35:16 +0000

    implement HOC for first window

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,23 +120,23 @@ const blogPosts = [
 const techStack = [
     {
         category: "Frontend",
-        items: ["React.js", "Next.js", "TypeScript"],
+        items: ["React.js", "TypeScript"],
     },
     {
         category: "Mobile",
-        items: ["React Native", "Expo"],
+        items: ["React Native", "Expo", "Flutter"],
     },
     {
         category: "Styling",
-        items: ["Tailwind CSS", "Sass", "CSS"],
+        items: ["Tailwind CSS", "Bootstrap", "CSS"],
     },
     {
         category: "Backend",
-        items: ["Node.js", "Express", "NestJS", "Hono"],
+        items: ["Node.js", "Python"],
     },
     {
         category: "Database",
-        items: ["MongoDB", "PostgreSQL"],
+        items: ["MongoDB", "SQL"],
     },
     {
         category: "Dev Tools",
